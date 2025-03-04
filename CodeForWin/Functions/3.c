eecce65ced1ed502305985e3f0f1a84628c06a8d
ShahdZayed22 <125815737+ShahdZayed22@users.noreply.github.com>
2025-03-04 23:04:25 +0200

    Create 3.c

---
+++ CodeForWin/Functions/3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Write a C program to find maximum and minimum between two numbers using functions.
+
+int findMax(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int findMin(int a, int b) {
+    return (a < b) ? a : b;
+}
+
+int main() {
+    
+    int num1, num2;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("Maximum: %d\n", findMax(num1, num2));
+    printf("Minimum: %d\n", findMin(num1, num2));
+
+    return 0;
+}
